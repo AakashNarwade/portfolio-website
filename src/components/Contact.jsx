e4ce72d2d2d894df140d8129ce871c38718d6117
AakashNarwade <aakashnarwade06@gmail.com>
2024-07-27 02:08:50 +0530

    responsive

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,12 +2,12 @@ const Contact = () => {
   return (
     <div
       name="contact"
-      className="w-full h-screen bg-gradient-to-b from-black to-gray-800 p-4 text-white"
+      className="w-full h-screen bg-gradient-to-b from-black to-gray-800 p-4 text-white md:pt-0 pt-32"
     >
       <div className="flex flex-col p-4 justify-center max-w-screen-lg mx-auto h-full">
-        <div className="pb-8">
-          <p className="text-4xl font-bold inline border-b-4 border-gray-500">
-            Contact
+        <div className="pb-8 text-center">
+          <p className="text-4xl font-bold inline border-gray-500 ">
+            Contact Me
           </p>
           <p className="py-6">Submit the form below to get in touch with me</p>
         </div>
--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -4,7 +4,7 @@ import javascript from "../assets/javascript.png";
 import reactImage from "../assets/react.png";
 import nextjs from "../assets/nextjs.png";
 // import graphql from "../assets/graphql.png";
-import github from "../assets/github.png";
+// import github from "../assets/github.png";
 import tailwind from "../assets/tailwind.png";
 import redux from "../assets/redux.png";
 import mern from "../assets/mern.jpg";
@@ -54,12 +54,12 @@ const Experience = () => {
       style: "shadow-[#764abc]",
     },
 
-    {
-      id: 8,
-      src: github,
-      title: "GitHub",
-      style: "shadow-gray-400",
-    },
+    // {
+    //   id: 8,
+    //   src: github,
+    //   title: "GitHub",
+    //   style: "shadow-gray-400",
+    // },
     {
       id: 9,
       src: mern,
@@ -71,7 +71,7 @@ const Experience = () => {
   return (
     <div
       name="experience"
-      className="bg-gradient-to-b from-gray-800 to-black w-full h-screen pt-24 md:pt-0 "
+      className="bg-gradient-to-b from-gray-800 to-black w-full h-screen py-24 md:py-0 "
     >
       <div className="max-w-screen-lg mx-auto p-4 flex flex-col justify-center w-full h-full text-white">
         <div>
